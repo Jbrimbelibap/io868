11efe252aa45ee0ad1f2f0f2f3afe97655cab2b4
Léonard de Vinci <90109439+Jbrimbelibap@users.noreply.github.com>
2023-07-17 14:44:47 +0200

    Added functions in spiffutils
    To remember the selected frequency for each page

--- include/spiffsutils.h
+++ include/spiffsutils.h
@@ -1,9 +1,12 @@
 #include "FS.h"
 #include "SPIFFS.h"
 
+double default_freq = 868.3; //Change the default frequency here
+double used_frequency; 
+
 #define FORMAT_SPIFFS_IF_FAILED true
 
-void listSPIFFS(const char * dirname, uint8_t levels){
+void listSPIFFS(const char * dirname, uint8_t levels){      //Function that lists all SPIFFS files, signals and saved frequencies. It is called on setup
     Serial.printf("Listing directory: %s\n", dirname);
 
     File root = SPIFFS.open(dirname);
@@ -34,7 +37,7 @@ void listSPIFFS(const char * dirname, uint8_t levels){
     }
 }
 
-void loadSPIFFS(const char * path, uint16_t *signal433_store, uint16_t size){
+void loadSPIFFS(const char * path, uint16_t *signal433_store, uint16_t size){       //Function that loads all 10 recorded signals. It is called on setup
     Serial.printf("Reading file: %s\n", path);
     File file = SPIFFS.open(path);
     if(!file || file.isDirectory()){
@@ -48,7 +51,7 @@ void loadSPIFFS(const char * path, uint16_t *signal433_store, uint16_t size){
     }
 }
 
-void storeSPIFFS(const char * path, uint16_t *signal433_store, uint16_t size){
+void storeSPIFFS(const char * path, uint16_t *signal433_store, uint16_t size){      //Function that stores the recorded signals using SPIFFS
     Serial.printf("Writing file: %s\r\n", path);
     File file = SPIFFS.open(path, FILE_WRITE);
     if(!file){
@@ -65,3 +68,25 @@ void storeSPIFFS(const char * path, uint16_t *signal433_store, uint16_t size){
 
 
 
+void saveFrequency(String freqname, double frequency) {  //Function to save in storage the selected frequency for a specific page
+  File freqFile = SPIFFS.open(freqname, "w");
+  if (!SPIFFS.exists(freqname)) {
+    Serial.println("Failed to open frequency file for writing");
+    return;
+  }
+  freqFile.print(frequency);
+  freqFile.close();
+}
+
+
+void readFrequency(String freqname) {       //Function that reads the saved frequency for a specific page
+  File freqFile = SPIFFS.open(freqname, "r");
+    if (!SPIFFS.exists(freqname)) {
+        Serial.println("Creating file with default frequency");
+        saveFrequency(freqname, default_freq);  //avoids having 0.00 by default when recording a single for the first time on a page
+        return;
+    }
+
+  used_frequency = freqFile.readString().toDouble();
+  freqFile.close();
+}
